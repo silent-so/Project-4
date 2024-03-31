48c571411b5b954802f1bae44ef9b0adc3ff54f4
darya160810 <d91554936@gmail.com>
2024-03-31 16:25:31 +0300

    Добавление своего товара

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from telebot import TeleBot
+import telebot
 
-token = '6529165154:AAH_5yolucmi9bjeXhVHip6Pc45Vm0WVyAo'
-bot = TeleBot(token)
+TOKEN = '7043497776:AAE3BnBDmwwvEGC3r1OxCibvURmNaRrC8Jw'
+bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Hello!')
+    bot.send_message(message.chat.id, 'Привет!Тут ты можешь добавлять свои товары /add')
+
+
+
+@bot.message_handler(commands=['add'])
+def add(message):
+    bot.send_message(message.chat.id, 'Введите название:')
+    bot.register_next_step_handler(message,item_name)
+
+def item_name(message):
+    bot.send_message(message.chat.id,'Хорошо,теперь отправь характеристику товара')
+    bot.register_next_step_handler(message, item_bio)
+def item_bio(message):
+    bot.send_message(message.chat.id,'Хорошо,теперь отправь фото товара')
+
+
+@bot.message_handler(content_types=['photo'])
+def photo(message):
+    bot.send_message(message.chat.id, 'Отлично!Добавляем ваш товар на склад...')
 
 bot.infinity_polling()
